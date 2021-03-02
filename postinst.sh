f31a4195b0afd97b85f2bb188a0a9cbd8634f597
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2021-03-02 15:49:00 +0100

    Auto net conf

--- postinst.sh
+++ postinst.sh
@@ -22,42 +22,17 @@ wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys
 chmod 600 ${SSH_KEY}
 chown -R ${USER} ${SSH_DIR}
 
-# Disable IPv6 for the current boot.
-sysctl net.ipv6.conf.all.disable_ipv6=1
-
-# Ensure IPv6 stays disabled.
-printf "\nnet.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-
-# Set the hostname, and then ensure it will resolve properly.
-printf "ubuntu2004.localdomain\n" > /etc/hostname
-printf "\n127.0.0.1 ubuntu2004.localdomain\n\n" >> /etc/hosts
-
-
 cat <<-EOF > /etc/netplan/01-netcfg.yaml
 network:
   version: 2
-  renderer: networkd
   ethernets:
     eth0:
       dhcp4: true
-      dhcp6: false
-      optional: true
-      nameservers:
-        addresses: [4.2.2.1, 4.2.2.2, 208.67.220.220]
+      dhcp-identifier: mac
 EOF
 
-# Apply the network plan configuration.
-netplan generate
-
-# Ensure a nameserver is being used that won't return an IP for non-existent domain names.
-sed -i -e "s/#DNS=.*/DNS=4.2.2.1 4.2.2.2 208.67.220.220/g" /etc/systemd/resolved.conf
-sed -i -e "s/#FallbackDNS=.*/FallbackDNS=/g" /etc/systemd/resolved.conf
-sed -i -e "s/#Domains=.*/Domains=/g" /etc/systemd/resolved.conf
-sed -i -e "s/#DNSSEC=.*/DNSSEC=yes/g" /etc/systemd/resolved.conf
-sed -i -e "s/#Cache=.*/Cache=yes/g" /etc/systemd/resolved.conf
-sed -i -e "s/#DNSStubListener=.*/DNSStubListener=yes/g" /etc/systemd/resolved.conf
-
-# Install ifplugd so we can monitor and auto-configure nics.
+sed -i 's/^#*\(send dhcp-client-identifier\).*$/\1 = hardware;/' /etc/dhcp/dhclient.conf
+sed -ie 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="net.ifnames=0 ipv6.disable=1 biosdevname=0"/' /etc/default/grub
 retry apt-get --assume-yes install ifplugd
 
 # Configure ifplugd to monitor the eth0 interface.
@@ -68,6 +43,3 @@ systemctl enable systemd-networkd.service
 
 # Ensure ifplugd also gets started, so the ethernet interface is monitored.
 systemctl enable ifplugd.service
-
-# Reboot onto the new kernel (if applicable).
-$(shutdown -r +1) &
