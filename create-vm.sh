104bc482c2bbe90c905df61d20d6f8da86349977
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2021-04-02 11:48:09 +0200

    Update script and packages

--- create-vm.sh
+++ create-vm.sh
@@ -28,7 +28,7 @@ virt-install \
  --extra-args "auto=true hostname=vagrant domain=${DOMAIN} console=tty0 console=ttyS0,115200n8 serial"
 
 # wait till installation finished
-while [ "$(virsh list | grep -c ${NAME})" != "0" ]
+while [[ "$(virsh list | grep -c "${NAME}")" != 0 ]]
 do
   sleep 5
 done
@@ -36,7 +36,7 @@ done
 # create the box
 qemu-img convert -m 12 -p -c -O qcow2 ${DISK} box.img
 tar cf - box.img metadata.json Vagrantfile | pigz > ${NAME}.box
-chmod 644 vagrant.box
+chmod 644 ${NAME}.box
 
 # clean up
 virsh undefine --nvram ${NAME}
